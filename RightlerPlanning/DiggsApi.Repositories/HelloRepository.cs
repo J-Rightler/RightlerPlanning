d0550621e8f20601406cdb408654f1b5bf83c47c
Diggs <jesserightler@gmail.com>
2015-11-03 06:15:35 -0600

    Just gettting it working,  Not sure if it actiually runs or not.

---
+++ RightlerPlanning/DiggsApi.Repositories/HelloRepository.cs
@@ -0,0 +1,17 @@
+﻿using DiggsApi.ServiceModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiggsApi.Repositories
+{
+    public class HelloRepository
+    {
+        public string SayHello(Hello input)
+        {
+            return string.Format("SayHello called with : {0}", input.Name);
+        }
+    }
+}
